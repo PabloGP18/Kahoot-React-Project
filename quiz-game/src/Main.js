63f66bd4775dfcbc7f9dbe61b5122c2950a1ce0c
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-10 14:50:27 +0200

    added the components Header/GameWindow/Footer to my component main and linked this to the class App

--- quiz-game/src/Main.js
+++ quiz-game/src/Main.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from "./Header";
+import GameWindow from "./GameWindow";
+import Footer from "./Footer";
 
 
 function Main (){
     return (
     <div className="main">
-        <Header  />
+        <div className="header">
+            <Header  />
+        </div>
+        <div className ="game-window">
+            <GameWindow />
+        </div>
+        <div className ="footer">
+            <Footer />
+        </div>
     </div>
     )
 }
