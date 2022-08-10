3dfd61681a81da6947f895fb96d1f10781c2dec5
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-10 14:51:19 +0200

    added extra components to create my quiz game

---
+++ quiz-game/src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {useEffect, useState} from "react";
+
+function Footer(){
+    return(
+        <span>Muchos aligos</span>
+    )
+}
+
+export default Footer
---
+++ quiz-game/src/GameWindow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {useEffect, useState} from "react";
+
+function GameWindow(){
+    return(
+        <span>aligo</span>
+    )
+}
+
+export default GameWindow
