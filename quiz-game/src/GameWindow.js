1a7e29e0c33e84ead3243dca042a9368f34b2807
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-10 16:16:39 +0200

    added a async function to display data in console

--- quiz-game/src/GameWindow.js
+++ quiz-game/src/GameWindow.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import {useEffect, useState} from "react";
 
-function GameWindow(){
-    return(
+function GameWindow() {
+    const url = "https://opentdb.com/api.php?amount=10&category=20"
+//something seems awfully hardcoded here, if only the user could change these settings!
+useEffect(() =>{getQuestions(url);},[]);
+    const getQuestions = async(url) => {
+        const res = await  fetch(url);
+        const data = await res.json();
+        console.log(data);
+        return data
+    }
+
+    return (
         <span>aligo</span>
     )
 }
