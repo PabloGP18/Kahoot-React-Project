5acfef1bd342c77efbc9dc7ca5abb93281956c3d
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-08-10 16:15:23 +0200

    made no changes yet

--- quiz-game/src/Footer.js
+++ quiz-game/src/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {useEffect, useState} from "react";
 
-function Footer(){
-    return(
-        <span>Muchos aligos</span>
+function Footer() {
+    return (
+        <footer>
+            <p>Author: Pablo GP< /p>
+            <p><a href="mailto:hege@example.com">hege@example.com< /a>< /p>
+        </footer>
     )
 }
-
 export default Footer
